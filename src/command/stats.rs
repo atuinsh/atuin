e2b15dcd27571208b8599e5978f8967492f1ae88
Ellie Huxtable <e@elm.sh>
2021-02-14 22:09:25 +0000

    Output tables, fix import blanks

--- src/command/stats.rs
+++ src/command/stats.rs
@@ -1,48 +1,98 @@
-use chrono::{Duration, Timelike, Utc};
+use std::collections::HashMap;
+
+use chrono::prelude::*;
+use chrono::{Duration, Utc};
 use chrono_english::{parse_date_string, Dialect};
-use eyre::Result;
+
+use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
+use eyre::{eyre, Result};
 use structopt::StructOpt;
 
 use crate::local::database::{Database, Sqlite};
+use crate::local::history::History;
 
 #[derive(StructOpt)]
 pub enum Cmd {
     #[structopt(
-        about="compute statistics per day of the week",
+        about="compute statistics for all of time",
+        aliases=&["d", "da"],
+    )]
+    All,
+
+    #[structopt(
+        about="compute statistics for a single day",
         aliases=&["d", "da"],
     )]
     Day { words: Vec<String> },
+}
+
+fn compute_stats(history: &[History]) -> Result<()> {
+    let mut commands = HashMap::<String, i64>::new();
+
+    for i in history {
+        *commands.entry(i.command.clone()).or_default() += 1;
+    }
+
+    let most_common_command = commands.iter().max_by(|a, b| a.1.cmp(b.1));
+
+    if most_common_command.is_none() {
+        return Err(eyre!("No commands found"));
+    }
+
+    let table = vec![
+        vec![
+            "Most used command".cell(),
+            most_common_command
+                .unwrap()
+                .0
+                .cell()
+                .justify(Justify::Right),
+        ],
+        vec![
+            "Commands ran".cell(),
+            history.len().to_string().cell().justify(Justify::Right),
+        ],
+        vec![
+            "Unique commands ran".cell(),
+            commands.len().to_string().cell().justify(Justify::Right),
+        ],
+    ]
+    .table()
+    .title(vec![
+        "Statistic".cell().bold(true),
+        "Value".cell().bold(true),
+    ])
+    .bold(true);
+
+    print_stdout(table)?;
+
+    Ok(())
 }
 
 impl Cmd {
     pub fn run(&self, db: &mut Sqlite) -> Result<()> {
         match self {
             Self::Day { words } => {
-                debug!("words len {}", words.len());
-
                 let words = if words.is_empty() {
                     String::from("yesterday")
                 } else {
                     words.join(" ")
                 };
 
-                let datetime = parse_date_string(words.as_str(), Utc::now(), Dialect::Us)?;
+                let start = parse_date_string(words.as_str(), Local::now(), Dialect::Us)?;
+                let end = start + Duration::days(1);
 
-                debug!("stats for {}", datetime);
+                let history = db.range(start.with_timezone(&Utc), end.with_timezone(&Utc))?;
 
-                let datetime = datetime
-                    .with_hour(0)
-                    .unwrap()
-                    .with_minute(0)
-                    .unwrap()
-                    .with_second(0)
-                    .unwrap();
+                compute_stats(&history)?;
 
-                let history = db.range(datetime, datetime + Duration::days(1))?;
+                Ok(())
+            }
 
-                for i in history {
-                    println!("{}", i.command);
-                }
+            Self::All => {
+                let history = db.list()?;
+
+                compute_stats(&history)?;
 
                 Ok(())
             }
