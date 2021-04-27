c11865f589f6813eae92c86f3d7a58ecbd06bebc
Ellie Huxtable <e@elm.sh>
2021-04-27 18:34:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ __atuin_install_arch(){
 __atuin_install_ubuntu(){
 	echo "Ubuntu detected"
 	# TODO: select correct AARCH too
-	ARTIFACT_URL="https://github.com/account/project/releases/download/$LATEST_VERSION/atuin_$LATEST_VERSION_amd64.deb"
+	ARTIFACT_URL="https://github.com/account/project/releases/download/$LATEST_VERSION/atuin_${LATEST_VERSION}_amd64.deb"
 
 	TEMP_DEB="$(mktemp)" &&
 	wget -O "$TEMP_DEB" $ARTIFACT_URL
