27d3d81afe3021e226b80a2c7734ed9a8c595641
Jannik <jannik.peters@posteo.de>
2021-09-24 18:24:59 +0200

    feat: allow input of credentials from stdin (#185)
    * feat: allow credential input from stdin for registration
    
    This changes the options for register to be optional. If arguments are
    not given, the program will ask for them interactively.
    
    * feat: allow credential input from stdin for login
    
    * style: apply cargo fmt

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -147,12 +147,7 @@ impl AtuinCmd {
                 logout::run();
                 Ok(())
             }
-            Self::Register(r) => register::run(
-                &client_settings,
-                r.username.as_str(),
-                r.email.as_str(),
-                r.password.as_str(),
-            ),
+            Self::Register(r) => register::run(&client_settings, r.username, r.email, r.password),
             Self::Key => {
                 let key = atuin_client::encryption::load_key(&client_settings)?;
                 println!("{}", atuin_client::encryption::encode_key(key)?);
