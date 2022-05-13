5c52392cfdf80928fd94be777ec354b40f2556ce
Bradley Noyes <b@noyes.dev>
2022-05-13 12:46:25 -0400

    Rewriting tests and using `Vec<u8>` instead of `String`
     - Rewriting tests to eliminate depencency on local file system
     - Using `Vec<u8>` for command strings instead of `String` to eliminate
       the utf8 errors i was seeing previously. Seems to be working.

--- atuin-client/src/import/zsh_histdb.rs
+++ atuin-client/src/import/zsh_histdb.rs
@@ -40,7 +40,7 @@ use chrono::{prelude::*, Utc};
 use async_trait::async_trait;
 use directories::UserDirs;
 use eyre::{eyre, Result};
-use sqlx::sqlite::SqlitePool;
+use sqlx::{Pool, sqlite::SqlitePool};
 
 use super::Importer;
 use crate::history::History;
@@ -57,7 +57,7 @@ pub struct HistDbEntry {
     pub start_time: NaiveDateTime,
     pub host: String,
     pub dir: String,
-    pub argv: String,
+    pub argv: Vec<u8>,
     pub duration: i64,
 }
 
@@ -65,7 +65,7 @@ impl From<HistDbEntry> for History {
     fn from(histdb_item: HistDbEntry) -> Self {
         History::new (
             DateTime::from_utc(histdb_item.start_time, Utc), // must assume UTC? 
-            histdb_item.argv.trim_end().to_string(),
+            String::from_utf8(histdb_item.argv).unwrap_or_else(|_e| String::from("")).trim_end().to_string(),
             histdb_item.dir,
             0, // assume 0, we have no way of knowing :(
             histdb_item.duration,
@@ -84,15 +84,19 @@ pub struct ZshHistDb {
 /// Read db at given file, return vector of entries.
 async fn hist_from_db(dbpath: PathBuf) -> Result<Vec<HistDbEntry>> {
     let pool = SqlitePool::connect(dbpath.to_str().unwrap()).await?;
-    let query = format!("select history.id,history.start_time,history.duration,places.host,places.dir,commands.argv from history left join commands on history.command_id = commands.rowid left join places on history.place_id = places.rowid order by history.start_time");
-    let histdb_vec : Vec<HistDbEntry> = sqlx::query_as::<_, HistDbEntry>(&query)
+    hist_from_db_conn(pool).await
+}
+
+async fn hist_from_db_conn(pool: Pool<sqlx::Sqlite>) -> Result<Vec<HistDbEntry>> {
+    let query = "select history.id,history.start_time,history.duration,places.host,places.dir,commands.argv from history left join commands on history.command_id = commands.rowid left join places on history.place_id = places.rowid order by history.start_time";
+    let histdb_vec : Vec<HistDbEntry> = sqlx::query_as::<_, HistDbEntry>(query)
             .fetch_all(&pool)
             .await?;
     Ok(histdb_vec)
 }
 
 impl ZshHistDb {
-    pub fn histpath() -> Result<PathBuf> {
+    pub fn histpath_candidate() -> PathBuf {
 
         // By default histdb database is `${HOME}/.histdb/zsh-history.db`
         // This can be modified by ${HISTDB_FILE}
@@ -101,10 +105,13 @@ impl ZshHistDb {
         //      typeset -g HISTDB_FILE="${HOME}/.histdb/zsh-history.db"
         let user_dirs = UserDirs::new().unwrap(); // should catch error here?
         let home_dir = user_dirs.home_dir();
-        let histdb_path = std::env::var("HISTDB_FILE")
+        std::env::var("HISTDB_FILE")
                             .as_ref()
                             .map(|x| Path::new(x).to_path_buf())
-                            .unwrap_or_else(|_err| home_dir.join(".histdb/zsh-history.db"));
+                            .unwrap_or_else(|_err| home_dir.join(".histdb/zsh-history.db"))
+    }
+    pub fn histpath() -> Result<PathBuf> {
+        let histdb_path = ZshHistDb::histpath_candidate();
         if histdb_path.exists() { Ok(histdb_path) }
         else {  Err(eyre!("Could not find history file. Try setting $HISTDB_FILE")) }
     }
@@ -140,22 +147,73 @@ mod test {
 
     use super::*;
     use std::env;
+    use sqlx::sqlite::SqlitePoolOptions;
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_env_vars() {
+        let test_env_db = "nonstd-zsh-history.db";
+        let key = "HISTDB_FILE";
+        env::set_var(key, test_env_db);
+
+        // test the env got set
+        assert_eq!(env::var(key).unwrap(), test_env_db.to_string());
+
+        // test histdb returns the proper db from previous step
+        let histdb_path = ZshHistDb::histpath_candidate();
+        assert_eq!(histdb_path.to_str().unwrap() , test_env_db);
+    }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn test_import() {
-        let test_db1 = "test_files/zsh-history.db";
-        let key = "HISTDB_FILE";
-        env::set_var(key, test_db1);
+        let pool: SqlitePool = SqlitePoolOptions::new()
+            .min_connections(2)
+            .connect(":memory:")
+            .await
+            .unwrap();
 
-        // test the env got set
-        assert_eq!(env::var(key).unwrap(), test_db1.to_string());
 
-        // test histdb returns the proper db from previous step
-        let histdb_path = ZshHistDb::histpath();
-        assert_eq!(histdb_path.unwrap().to_str().unwrap() , test_db1);
+        // sql dump directly from a test database.
+        let db_sql = r#"
+        PRAGMA foreign_keys=OFF;
+        BEGIN TRANSACTION;
+        CREATE TABLE commands (id integer primary key autoincrement, argv text, unique(argv) on conflict ignore);
+        INSERT INTO commands VALUES(1,'pwd');
+        INSERT INTO commands VALUES(2,'curl google.com');
+        INSERT INTO commands VALUES(3,'bash');
+        CREATE TABLE places   (id integer primary key autoincrement, host text, dir text, unique(host, dir) on conflict ignore);
+        INSERT INTO places VALUES(1,'mbp16.local','/home/noyez');
+        CREATE TABLE history  (id integer primary key autoincrement,
+                               session int,
+                               command_id int references commands (id),
+                               place_id int references places (id),
+                               exit_status int,
+                               start_time int,
+                               duration int);
+        INSERT INTO history VALUES(1,0,1,1,0,1651497918,1);
+        INSERT INTO history VALUES(2,0,2,1,0,1651497923,1);
+        INSERT INTO history VALUES(3,0,3,1,NULL,1651497930,NULL);
+        DELETE FROM sqlite_sequence;
+        INSERT INTO sqlite_sequence VALUES('commands',3);
+        INSERT INTO sqlite_sequence VALUES('places',3);
+        INSERT INTO sqlite_sequence VALUES('history',3);
+        CREATE INDEX hist_time on history(start_time);
+        CREATE INDEX place_dir on places(dir);
+        CREATE INDEX place_host on places(host);
+        CREATE INDEX history_command_place on history(command_id, place_id);
+        COMMIT; "#;
+
+
+        sqlx::query(db_sql)
+            .execute(&pool)
+            .await
+            .unwrap();
+
+
+
 
         // test histdb iterator
-        let histdb = ZshHistDb::new().await.unwrap();
+        let histdb_vec = hist_from_db_conn(pool).await.unwrap();
+        let histdb = ZshHistDb { histdb: histdb_vec };
+
         println!("h: {:#?}", histdb.histdb);
         println!("counter: {:?}", histdb.histdb.len());
         for i in histdb.histdb {
