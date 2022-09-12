055648c5fd1eecfbbeef616018bd9cc2ecf42796
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-09-12 17:45:14 +0100

    segment

--- src/command/client/search/history_list.rs
+++ src/command/client/search/history_list.rs
@@ -33,12 +33,9 @@ impl ListState {
     }
 }
 
-const MAX_LENGTH: usize = " > 123ms 59s ago".len();
-
 impl<'a> StatefulWidget for HistoryList<'a> {
     type State = ListState;
 
-    #[allow(clippy::cast_possible_truncation)] // we do not expect MAX_LENGTH or ago.len() to overflow a u16
     fn render(mut self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         buf.set_style(area, self.style);
         let list_area = match self.block.take() {
@@ -62,7 +59,7 @@ impl<'a> StatefulWidget for HistoryList<'a> {
         let (start, end) = self.get_items_bounds(state.selected, state.offset, list_height);
         state.offset = start;
 
-        let mut current_height = 0;
+        let mut y = list_area.bottom();
         for (i, item) in self
             .history
             .iter()
@@ -70,37 +67,20 @@ impl<'a> StatefulWidget for HistoryList<'a> {
             .skip(state.offset)
             .take(end - start)
         {
-            current_height += 1;
-            let (mut x, y) = (list_area.left(), list_area.bottom() - current_height);
+            y += 1;
+            let mut s = DrawState {
+                buf,
+                width: list_area.width as usize,
+                x: list_area.left(),
+                y,
+                i,
+                state,
+            };
 
-            // these encode the slices of `" > "`, `" {n} "`, or `"   "` in a compact form.
-            // Yes, this is a hack, but it makes me feel happy
-            let slices = " > 1 2 3 4 5 6 7 8 9   ";
-            let slice_index = i.checked_sub(state.selected).unwrap_or(10).min(10) * 2;
-            let ptr = &slices[slice_index..slice_index + 3];
-            (x, _) = buf.set_stringn(x, y, ptr, list_area.width as usize, Style::default());
-
-            let status = Style::default().fg(if item.success() {
-                Color::Green
-            } else {
-                Color::Red
-            });
-            let dur = duration(item);
-            buf.set_stringn(x, y, &dur, (list_area.width - x) as usize, status);
-
-            let ago = ago(item);
-            x = list_area.left() + (MAX_LENGTH - ago.len()) as u16;
-            let style = Style::default().fg(Color::Blue);
-            (x, _) = buf.set_stringn(x, y, &ago, (list_area.width - x) as usize, style);
-
-            let mut style = Style::default();
-            if slice_index == 0 {
-                style = style.fg(Color::Red).add_modifier(Modifier::BOLD);
-            }
-            for section in item.command.split_ascii_whitespace() {
-                x += 1;
-                (x, _) = buf.set_stringn(x, y, &section, (list_area.width - x) as usize, style);
-            }
+            s.index();
+            s.duration(item);
+            s.time(list_area.left(), item);
+            s.command(item);
         }
     }
 }
@@ -132,12 +112,77 @@ impl<'a> HistoryList<'a> {
     }
 }
 
+struct DrawState<'a> {
+    buf: &'a mut Buffer,
+    width: usize,
+    x: u16,
+    y: u16,
+    i: usize,
+    state: &'a ListState,
+}
+
+// longest line prefix I could come up with
+const PREFIX_LENGTH: usize = " > 123ms 59s ago".len();
+
+impl DrawState<'_> {
+    fn index(&mut self) {
+        // these encode the slices of `" > "`, `" {n} "`, or `"   "` in a compact form.
+        // Yes, this is a hack, but it makes me feel happy
+        static SLICES: &str = " > 1 2 3 4 5 6 7 8 9   ";
+        let i = self.i.checked_sub(self.state.selected);
+        let i = i.unwrap_or(10).min(10) * 2;
+        self.draw(&SLICES[i..i + 3], Style::default());
+    }
+
+    fn duration(&mut self, h: &History) {
+        let status = Style::default().fg(if h.success() {
+            Color::Green
+        } else {
+            Color::Red
+        });
+        let dur = duration(h);
+        self.draw(&dur, status);
+    }
+
+    #[allow(clippy::cast_possible_truncation)] // we do not expect PREFIX_LENGTH or ago.len() to overflow a u16
+    fn time(&mut self, left: u16, h: &History) {
+        let status = Style::default().fg(Color::Blue);
+        let time = time(h);
+
+        // pad the time a little bit before we write. this aligns things nicely
+        let x = left + (PREFIX_LENGTH - time.len()) as u16;
+        self.width -= (x - self.x) as usize;
+        self.x = x;
+
+        self.draw(&time, status);
+    }
+
+    fn command(&mut self, h: &History) {
+        let mut style = Style::default();
+        if self.i == self.state.selected {
+            style = style.fg(Color::Red).add_modifier(Modifier::BOLD);
+        }
+
+        for section in h.command.split_ascii_whitespace() {
+            self.x += 1;
+            self.width -= 1;
+            self.draw(section, style);
+        }
+    }
+
+    fn draw(&mut self, s: &str, style: Style) {
+        let x = self.buf.set_stringn(self.x, self.y, s, self.width, style).0;
+        self.width -= (x - self.x) as usize;
+        self.x = x;
+    }
+}
+
 fn duration(h: &History) -> String {
     let duration = Duration::from_nanos(u64::try_from(h.duration).unwrap_or(0));
     format_duration(duration)
 }
 
-fn ago(h: &History) -> String {
+fn time(h: &History) -> String {
     let ago = chrono::Utc::now() - h.timestamp;
 
     // Account for the chance that h.timestamp is "in the future"
