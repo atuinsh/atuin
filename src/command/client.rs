3f925d829505e4019644e6d9f881937843a375c9
c_14 <git@c-14.de>
2022-09-24 17:34:48 +0200

    Allow stateless commands to be run without config/database
    Fixes an issue where gen-completions fails trying to create a config
    directory in restrained build environments/distribution.

--- src/command/client.rs
+++ src/command/client.rs
@@ -18,7 +18,7 @@ mod stats;
 
 #[derive(Subcommand)]
 #[clap(infer_subcommands = true)]
-pub enum Cmd {
+pub enum StatefulCmd {
     /// Manipulate shell history
     #[clap(subcommand)]
     History(history::Cmd),
@@ -30,15 +30,26 @@ pub enum Cmd {
     /// Calculate statistics for your history
     Stats(stats::Cmd),
 
+    /// Interactive history search
+    Search(search::Cmd),
+
+    #[cfg(feature = "sync")]
+    #[clap(flatten)]
+    Sync(sync::Cmd),
+}
+
+#[derive(Subcommand)]
+#[clap(infer_subcommands = true)]
+pub enum Cmd {
     /// Output shell setup
     #[clap(subcommand)]
     Init(init::Cmd),
 
+    #[clap(flatten)]
+    Stateful(StatefulCmd),
+
     /// Generate a UUID
     Uuid,
-
-    /// Interactive history search
-    Search(search::Cmd),
 
     /// Generate shell completions
     GenCompletions {
@@ -50,17 +61,10 @@ pub enum Cmd {
         #[clap(long, short)]
         out_dir: Option<String>,
     },
-
-    #[cfg(feature = "sync")]
-    #[clap(flatten)]
-    Sync(sync::Cmd),
 }
 
-impl Cmd {
-    #[tokio::main(flavor = "current_thread")]
+impl StatefulCmd {
     pub async fn run(self) -> Result<()> {
-        pretty_env_logger::init();
-
         let settings = Settings::new().wrap_err("could not load client settings")?;
 
         let db_path = PathBuf::from(settings.db_path.as_str());
@@ -70,11 +74,24 @@ impl Cmd {
             Self::History(history) => history.run(&settings, &mut db).await,
             Self::Import(import) => import.run(&mut db).await,
             Self::Stats(stats) => stats.run(&mut db, &settings).await,
+            Self::Search(search) => search.run(&mut db, &settings).await,
+            #[cfg(feature = "sync")]
+            Self::Sync(sync) => sync.run(settings, &mut db).await,
+        }
+    }
+}
+
+impl Cmd {
+    #[tokio::main(flavor = "current_thread")]
+    pub async fn run(self) -> Result<()> {
+        pretty_env_logger::init();
+
+        match self {
             Self::Init(init) => {
                 init.run();
                 Ok(())
             }
-            Self::Search(search) => search.run(&mut db, &settings).await,
+            Self::Stateful(cmd) => cmd.run().await,
             Self::Uuid => {
                 println!("{}", uuid_v4());
                 Ok(())
@@ -98,8 +115,6 @@ impl Cmd {
 
                 Ok(())
             }
-            #[cfg(feature = "sync")]
-            Self::Sync(sync) => sync.run(settings, &mut db).await,
         }
     }
 }
