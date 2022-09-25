7edec216467536e3647619ec36c782d0c57ed844
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-09-25 10:18:09 +0100

    move non-db commands up to core subcommands

--- src/command/client.rs
+++ src/command/client.rs
@@ -1,24 +1,21 @@
 use std::path::PathBuf;
 
-use clap::{CommandFactory, Subcommand};
-use clap_complete::{generate, generate_to, Shell};
+use clap::Subcommand;
 use eyre::{Result, WrapErr};
 
 use atuin_client::{database::Sqlite, settings::Settings};
-use atuin_common::utils::uuid_v4;
 
 #[cfg(feature = "sync")]
 mod sync;
 
 mod history;
 mod import;
-mod init;
 mod search;
 mod stats;
 
 #[derive(Subcommand)]
 #[clap(infer_subcommands = true)]
-pub enum StatefulCmd {
+pub enum Cmd {
     /// Manipulate shell history
     #[clap(subcommand)]
     History(history::Cmd),
@@ -38,32 +35,8 @@ pub enum StatefulCmd {
     Sync(sync::Cmd),
 }
 
-#[derive(Subcommand)]
-#[clap(infer_subcommands = true)]
-pub enum Cmd {
-    /// Output shell setup
-    #[clap(subcommand)]
-    Init(init::Cmd),
-
-    #[clap(flatten)]
-    Stateful(StatefulCmd),
-
-    /// Generate a UUID
-    Uuid,
-
-    /// Generate shell completions
-    GenCompletions {
-        /// Set the shell for generating completions
-        #[clap(long, short)]
-        shell: Shell,
-
-        /// Set the output directory
-        #[clap(long, short)]
-        out_dir: Option<String>,
-    },
-}
-
-impl StatefulCmd {
+impl Cmd {
+    #[tokio::main(flavor = "current_thread")]
     pub async fn run(self) -> Result<()> {
         let settings = Settings::new().wrap_err("could not load client settings")?;
 
@@ -77,44 +50,6 @@ impl StatefulCmd {
             Self::Search(search) => search.run(&mut db, &settings).await,
             #[cfg(feature = "sync")]
             Self::Sync(sync) => sync.run(settings, &mut db).await,
-        }
-    }
-}
-
-impl Cmd {
-    #[tokio::main(flavor = "current_thread")]
-    pub async fn run(self) -> Result<()> {
-        pretty_env_logger::init();
-
-        match self {
-            Self::Init(init) => {
-                init.run();
-                Ok(())
-            }
-            Self::Stateful(cmd) => cmd.run().await,
-            Self::Uuid => {
-                println!("{}", uuid_v4());
-                Ok(())
-            }
-            Self::GenCompletions { shell, out_dir } => {
-                let mut cli = crate::Atuin::command();
-
-                match out_dir {
-                    Some(out_dir) => {
-                        generate_to(shell, &mut cli, env!("CARGO_PKG_NAME"), &out_dir)?;
-                    }
-                    None => {
-                        generate(
-                            shell,
-                            &mut cli,
-                            env!("CARGO_PKG_NAME"),
-                            &mut std::io::stdout(),
-                        );
-                    }
-                }
-
-                Ok(())
-            }
         }
     }
 }
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,4 +1,5 @@
-use clap::Subcommand;
+use clap::{CommandFactory, Subcommand};
+use clap_complete::{generate, generate_to, Shell};
 use eyre::Result;
 
 #[cfg(feature = "client")]
@@ -7,8 +8,9 @@ mod client;
 #[cfg(feature = "server")]
 mod server;
 
+mod init;
+
 #[derive(Subcommand)]
-#[clap(infer_subcommands = true)]
 pub enum AtuinCmd {
     #[cfg(feature = "client")]
     #[clap(flatten)]
@@ -18,6 +20,24 @@ pub enum AtuinCmd {
     #[cfg(feature = "server")]
     #[clap(subcommand)]
     Server(server::Cmd),
+
+    /// Output shell setup
+    #[clap(subcommand)]
+    Init(init::Cmd),
+
+    /// Generate a UUID
+    Uuid,
+
+    /// Generate shell completions
+    GenCompletions {
+        /// Set the shell for generating completions
+        #[clap(long, short)]
+        shell: Shell,
+
+        /// Set the output directory
+        #[clap(long, short)]
+        out_dir: Option<String>,
+    },
 }
 
 impl AtuinCmd {
@@ -27,6 +47,33 @@ impl AtuinCmd {
             Self::Client(client) => client.run(),
             #[cfg(feature = "server")]
             Self::Server(server) => server.run(),
+            Self::Init(init) => {
+                init.run();
+                Ok(())
+            }
+            Self::Uuid => {
+                println!("{}", atuin_common::utils::uuid_v4());
+                Ok(())
+            }
+            Self::GenCompletions { shell, out_dir } => {
+                let mut cli = crate::Atuin::command();
+
+                match out_dir {
+                    Some(out_dir) => {
+                        generate_to(shell, &mut cli, env!("CARGO_PKG_NAME"), &out_dir)?;
+                    }
+                    None => {
+                        generate(
+                            shell,
+                            &mut cli,
+                            env!("CARGO_PKG_NAME"),
+                            &mut std::io::stdout(),
+                        );
+                    }
+                }
+
+                Ok(())
+            }
         }
     }
 }
