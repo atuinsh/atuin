ff4606b63a9f361b614ffbf5ec7ac1eef3fd78a2
Alpha Chen <alpha@kejadlen.dev>
2023-02-26 23:10:52 -0800

    Fix editURL for docs (#732)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ellie/atuin/tree/main/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ellie/atuin/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
