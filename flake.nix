f18f0d7e35a23898afd2857f763ae2650f2bf867
0x4A6F <0x4A6F@users.noreply.github.com>
2023-02-28 21:15:31 +0100

    nix: add flake-compat

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
   outputs = {
     self,
