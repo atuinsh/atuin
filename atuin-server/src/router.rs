bd72a428f17099b40a4a12c7cc813cd5b9d50152
YummyOreo <bobgim20@gmail.com>
2023-03-01 17:13:24 -0600

    removes comment

--- atuin-server/src/router.rs
+++ atuin-server/src/router.rs
@@ -82,5 +82,4 @@ pub fn router<DB: Database + Clone + Send + Sync + 'static>(
     .fallback(teapot)
     .with_state(AppState { database, settings })
     .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
-    // .layer(axum::Extension(settings))
 }
