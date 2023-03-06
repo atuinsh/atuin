3e4013f953074e139aa6042565d39bb90793eef1
Luke Karrys <luke@lukekarrys.com>
2023-03-06 13:06:26 -0700

    fixup! fix(client): always read session_path from settings

--- src/command/client/sync/logout.rs
+++ src/command/client/sync/logout.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use eyre::{Context, Result};
 use fs_err::remove_file;
 
-use atuin_client::{settings::Settings};
+use atuin_client::settings::Settings;
 
 pub fn run(settings: &Settings) -> Result<()> {
     let session_path = settings.session_path.as_str();
