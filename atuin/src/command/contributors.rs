9e3fa8b88ac3c67c5b544475b624b94895220ddd
Ellie Huxtable <ellie@elliehuxtable.com>
2023-05-28 19:39:45 +0100

    Release v15.0.0 (#995)
    * Release v15.0.0
    
    * Draft post
    
    * Update contributors

--- atuin/src/command/contributors.rs
+++ atuin/src/command/contributors.rs
@@ -1,8 +1,15 @@
 const CONTRIBUTORS: &str = r#"
+0x4A6F 
+Aleks Bunin 
+Alpha Chen <alpha@kejadlen.dev>
+Austin Schey <aschey13@gmail.com>
 Baptiste 
+Ben J <bdavjones@gmail.com>
 Benjamin Vergnaud 
 Brad Robel-Forrest <brad@bitpony.com>
+Brian Kung 
 Bruce Huang <helbingxxx@gmail.com>
+Ch. (Chanwhi Choi) <ccwpc@hanmail.net>
 Conrad Ludgate <conradludgate@gmail.com>
 CosmicHorror <LovecraftianHorror@pm.me>
 Daniel <daniel.hub@outlook.de>
@@ -12,62 +19,93 @@ Eric Ripa <eric@ripa.io>
 Erwin Kroon 
 Evan Purkhiser <evanpurkhiser@gmail.com>
 Frank Hamand <frankhamand@gmail.com>
+Gokul <appu.yess@gmail.com>
+Hamza Hamud 
 Herby Gillot <herby.gillot@gmail.com>
+Hilmar Wiegand <me@hwgnd.de>
 Ian Smith <ismith@mit.edu>
 Ilkin Bayramli 
-Violet Shreve <github@shreve.io>
 Jakob Schrettenbrunner <dev@schrej.net>
-Jakob-Niklas See <github@nwex.de>
 Jakub Jirutka <jakub@jirutka.cz>
 Jakub Panek <me@panekj.dev>
 Jamie Quigley <jamie@quigley.xyz>
+Jan Larres <jan@majutsushi.net>
 Jannik <jannik.peters@posteo.de>
 Jerome Ducret <jdiphone34@gmail.com>
 Johannes Baiter <johannes.baiter@gmail.com>
+János Illés <ijanos@gmail.com>
+Kjetil Jørgensen <kjetijor+github@gmail.com>
 Klas Mellbourn <klas@mellbourn.net>
-Laurent le Beau-Martin
+Krithic Kumar 
+Krut Patel <kroot.patel@gmail.com>
+Laurent le Beau-Martin 
 Lucas Burns 
 Lucy <lucy@absolucy.moe>
 Luke Baker <lukebaker@gmail.com>
+Luke Karrys <luke@lukekarrys.com>
 Manel Vilar <manelvf@gmail.com>
+Marijan Smetko <msmetko@msmetko.xyz>
 Mark Wotton <mwotton@gmail.com>
 Martin Indra <martin.indra@mgn.cz>
 Martin Junghanns <m.junghanns@mailbox.org>
 Mat Jones <mat@mjones.network>
+Maurice Escher <maurice.escher@sap.com>
+Maxim Uvarov 
 Michael Bianco <iloveitaly@gmail.com>
 Michael Mior <michael.mior@gmail.com>
+Nico Kokonas <nico@nicomee.com>
+Niklas Hambüchen <mail@nh2.me>
 Omer Katz <omer.drow@gmail.com>
 Orhun Parmaksız <orhunparmaksiz@gmail.com>
 Patrick 
 Patrick Decat <pdecat@gmail.com>
 Patrick Jackson <patrick@jackson.dev>
+Per Modin 
 Plamen Dimitrov <pdimitrov@pevogam.com>
+Qiming Xu 
+Richard de Boer <github@tubul.net>
+Robin Millette <robin@millette.info>
 Sam Edwards <sam@samedwards.ca>
 Sam Lanning <sam@samlanning.com>
 Sandro <sandro.jaeckel@gmail.com>
 Satyarth Sampath <satyarth.23@gmail.com>
 Simon Elsbrock <simon@iodev.org>
+Steve Kemp <steve@steve.org.uk>
+Steven Xu 
 Tobias Hunger <tobias.hunger@gmail.com>
+Tom Cammann <cammann.tom@gmail.com>
 Trygve Aaberge <trygveaa@gmail.com>
 TymanWasTaken <tbeckman530@gmail.com>
 Ubiquitous Photon 
+Violet Shreve <github@shreve.io>
+Vladislav Stepanov <8uk.8ak@gmail.com>
+VuiMuich <jm.spam@gmx.net>
 Webmaster At Cosmic DNA 
 Will Fancher <elvishjerricco@gmail.com>
+WindSoilder <WindSoilder@outlook.com>
+Yannick Ulrich <yannick.ulrich@durham.ac.uk>
 Yolo <noah.chang@outlook.com>
+YummyOreo <bobgim20@gmail.com>
 Yuvi Panda <yuvipanda@gmail.com>
 ZhiHong Li <joker_lizhih@163.com>
 avinassh 
 b3nj5m1n 
 c-14 <git@c-14.de>
+cyqsimon 
+dependabot[bot] 
 frukto <fruktopus@gmail.com>
+jean-santos <ewqjean@gmail.com>
 jean-santos <jeanpnsantos@gmail.com>
 lchausmann <jazz-github@zqz.dk>
 mb6ockatf 
 morguldir <morguldir@protonmail.com>
-mundry
+mundry 
+networkException <git@nwex.de>
 noyez <noyez@ithryn.net>
+onkelT2 
+postmath 
 wpbrz 
-xfzv
+xfzv 
 "#;
 
 pub fn run() {
