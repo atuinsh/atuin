79edc4ce99aad88ad2588b7ff064477857d38c39
Ellie Huxtable <ellie@elliehuxtable.com>
2023-07-26 08:25:53 +0100

    Serve docusaurus on /docs/ (#1117)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://atuin.sh',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
