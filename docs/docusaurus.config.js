37b17023f6f1083214f4e9dc3394cf0934cfcaa9
Ellie Huxtable <ellie@elliehuxtable.com>
2023-07-26 08:41:13 +0100

    Try adjusting docs root

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -40,6 +40,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
