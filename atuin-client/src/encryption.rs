6736ed425c5689e46dd1b959f68a32d8713547a7
Ellie Huxtable <ellie@elliehuxtable.com>
2023-08-02 20:44:54 +0100

    Remove debug output

--- atuin-client/src/encryption.rs
+++ atuin-client/src/encryption.rs
@@ -147,8 +147,7 @@ fn encode(h: &History) -> Result<Vec<u8>> {
     encode::write_str(&mut output, &h.id)?;
     encode::write_str(
         &mut output,
-        &dbg!(h
-            .timestamp
+        &(h.timestamp
             .to_rfc3339_opts(chrono::SecondsFormat::AutoSi, true)),
     )?;
     encode::write_sint(&mut output, h.duration)?;
