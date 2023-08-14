609a13eb6d539904148b63942335502e1943102c
Ellie Huxtable <ellie@elliehuxtable.com>
2023-08-14 09:48:11 +0100

    Fix nix build
    I forgot nix builds in a sandbox, so my laziness earlier meant that the
    nix build fails - sandbox has no git!

--- atuin/build.rs
+++ atuin/build.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
 fn main() {
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
+    let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
 
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    let sha = match output {
+        Ok(sha) => String::from_utf8(sha.stdout).unwrap(),
+        Err(_) => String::from("NO_GIT"),
+    };
+
+    println!("cargo:rustc-env=GIT_HASH={}", sha);
 }
