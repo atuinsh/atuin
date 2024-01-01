e14299d6236798cfdf60f70d1ba96fb4a1e38b4b
Ellie Huxtable <ellie@elliehuxtable.com>
2024-01-01 19:39:40 +0000

    Update atuin/src/shell/atuin.zsh
    Co-authored-by: Patrick Jackson <patrick@jackson.dev>

--- atuin/src/shell/atuin.zsh
+++ atuin/src/shell/atuin.zsh
@@ -16,7 +16,7 @@ autoload -U add-zsh-hook
 _zsh_autosuggest_strategy_atuin_top() {
     suggestion=$(atuin search --cmd-only --limit 1 --search-mode prefix $1)
 }
-ZSH_AUTOSUGGEST_STRATEGY=atuin_top
+ZSH_AUTOSUGGEST_STRATEGY=("atuin_top" "${ZSH_AUTOSUGGEST_STRATEGY[@]}")
 
 export ATUIN_SESSION=$(atuin uuid)
 
