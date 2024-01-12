4d41a741f0a47904a75db2981f5295732f7549c5
Ellie Huxtable <ellie@elliehuxtable.com>
2024-01-12 18:52:39 +0000

    fix(perm): set umask 077 (#1554)
    This ensures no other user can read shell history data
    
    Resolves #1250

--- atuin/src/command/mod.rs
+++ atuin/src/command/mod.rs
@@ -2,6 +2,8 @@ use clap::{CommandFactory, Subcommand};
 use clap_complete::{generate, generate_to, Shell};
 use eyre::Result;
 
+use rustix::{fs::Mode, process::umask};
+
 #[cfg(feature = "client")]
 mod client;
 
@@ -46,6 +48,11 @@ pub enum AtuinCmd {
 
 impl AtuinCmd {
     pub fn run(self) -> Result<()> {
+        // set umask before we potentially open/create files
+        // or in other words, 077. Do not allow any access to any other user
+        let mode = Mode::RWXG | Mode::RWXO;
+        umask(mode);
+
         match self {
             #[cfg(feature = "client")]
             Self::Client(client) => client.run(),
