8001e2673b109da414441dad79cc904b0ea9805a
YummyOreo <bobgim20@gmail.com>
2024-01-13 11:40:15 -0600

    fix(windows): disables unix specific stuff for windows (windows has this on by default iirc)

--- atuin/src/command/mod.rs
+++ atuin/src/command/mod.rs
@@ -2,6 +2,7 @@ use clap::{CommandFactory, Subcommand};
 use clap_complete::{generate, generate_to, Shell};
 use eyre::Result;
 
+#[cfg(not(target_family = "windows"))]
 use rustix::{fs::Mode, process::umask};
 
 #[cfg(feature = "client")]
@@ -48,10 +49,14 @@ pub enum AtuinCmd {
 
 impl AtuinCmd {
     pub fn run(self) -> Result<()> {
-        // set umask before we potentially open/create files
-        // or in other words, 077. Do not allow any access to any other user
-        let mode = Mode::RWXG | Mode::RWXO;
-        umask(mode);
+
+        #[cfg(not(target_family = "windows"))]
+        {
+            // set umask before we potentially open/create files
+            // or in other words, 077. Do not allow any access to any other user
+            let mode = Mode::RWXG | Mode::RWXO;
+            umask(mode);
+        }
 
         match self {
             #[cfg(feature = "client")]
