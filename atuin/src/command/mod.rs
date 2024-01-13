2376f760dd67205b9ee3bc7833cc2a65c753bea4
YummyOreo <bobgim20@gmail.com>
2024-01-13 11:44:06 -0600

    refactor: use windows not target_family

--- atuin/src/command/mod.rs
+++ atuin/src/command/mod.rs
@@ -2,7 +2,7 @@ use clap::{CommandFactory, Subcommand};
 use clap_complete::{generate, generate_to, Shell};
 use eyre::Result;
 
-#[cfg(not(target_family = "windows"))]
+#[cfg(not(windows))]
 use rustix::{fs::Mode, process::umask};
 
 #[cfg(feature = "client")]
@@ -49,8 +49,7 @@ pub enum AtuinCmd {
 
 impl AtuinCmd {
     pub fn run(self) -> Result<()> {
-
-        #[cfg(not(target_family = "windows"))]
+        #[cfg(not(windows))]
         {
             // set umask before we potentially open/create files
             // or in other words, 077. Do not allow any access to any other user
