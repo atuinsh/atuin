bcbf72cd6a141d51f8d676bce5ad5ebdef446088
Koichi Murase <myoga.murase@gmail.com>
2024-01-14 15:05:28 +0900

    fix(search): fix invisible tab title

--- atuin/src/command/client/search/interactive.rs
+++ atuin/src/command/client/search/interactive.rs
@@ -469,7 +469,7 @@ impl State {
             .block(Block::default().borders(Borders::NONE))
             .select(self.tab_index)
             .style(Style::default())
-            .highlight_style(Style::default().bold().on_black());
+            .highlight_style(Style::default().bold().white().on_black());
 
         f.render_widget(tabs, tabs_chunk);
 
