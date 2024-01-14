dd19442b8ecfa27cc4cc941840f0c1ccf03fc7b8
Matt Godbolt <matt@godbolt.org>
2024-01-14 14:51:43 -0600

    Fix fish integration on older fishes
    - On fish 3.3 the bash-style `$(...)` doesn't work, we should use `(...)`
    - Also quoting `"(moo)"` on older fishes gives a literal `moo`
    - The result of a `(subcommand)` is a single token, so no need to quote it, anyway
    
    Tested by making the change, executing `cargo run -- init fish --disable-up-arrow` and then
    executing that shell script on a system with fish 3.3, as well as 3.6 and observing both still work.
    
    Fixes #1562.

--- atuin/src/shell/atuin.fish
+++ atuin/src/shell/atuin.fish
@@ -19,11 +19,11 @@ function _atuin_postexec --on-event fish_postexec
 end
 
 function _atuin_search
-    set -l ATUIN_H "$(ATUIN_SHELL_FISH=t ATUIN_LOG=error atuin search $argv -i -- (commandline -b) 3>&1 1>&2 2>&3)"
+    set -l ATUIN_H (ATUIN_SHELL_FISH=t ATUIN_LOG=error atuin search $argv -i -- (commandline -b) 3>&1 1>&2 2>&3)
 
     if test -n "$ATUIN_H"
         if string match --quiet '__atuin_accept__:*' "$ATUIN_H"
-          set -l ATUIN_HIST "$(string replace "__atuin_accept__:" "" -- "$ATUIN_H")"
+          set -l ATUIN_HIST (string replace "__atuin_accept__:" "" -- "$ATUIN_H")
           commandline -r "$ATUIN_HIST"
           commandline -f repaint
           commandline -f execute
