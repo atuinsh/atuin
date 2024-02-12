87e19df9c5d8c2a7438c93c67b4590df722fb95c
jfmontanaro <jfmonty2@gmail.com>
2024-02-12 02:32:07 -0800

    feat: Add xonsh history import (#1678)
    * add importers for xonsh JSON files and SQLite db
    
    * rustfmt xonsh importers
    
    * remove env-dependent tests from xonsh importers
    
    * pass xonsh_data_dir into path resolver instead of looking up in env
    
    * review: run format
    
    * review: fix clippy errors
    
    ---------
    
    Co-authored-by: Ellie Huxtable <ellie@elliehuxtable.com>

--- atuin/src/command/client/import.rs
+++ atuin/src/command/client/import.rs
@@ -9,8 +9,8 @@ use atuin_client::{
     database::Database,
     history::History,
     import::{
-        bash::Bash, fish::Fish, nu::Nu, nu_histdb::NuHistDb, resh::Resh, zsh::Zsh,
-        zsh_histdb::ZshHistDb, Importer, Loader,
+        bash::Bash, fish::Fish, nu::Nu, nu_histdb::NuHistDb, resh::Resh, xonsh::Xonsh,
+        xonsh_sqlite::XonshSqlite, zsh::Zsh, zsh_histdb::ZshHistDb, Importer, Loader,
     },
 };
 
@@ -34,6 +34,10 @@ pub enum Cmd {
     Nu,
     /// Import history from the nu history file
     NuHistDb,
+    /// Import history from xonsh json files
+    Xonsh,
+    /// Import history from xonsh sqlite db
+    XonshSqlite,
 }
 
 const BATCH_SIZE: usize = 100;
@@ -97,6 +101,8 @@ impl Cmd {
             Self::Fish => import::<Fish, DB>(db).await,
             Self::Nu => import::<Nu, DB>(db).await,
             Self::NuHistDb => import::<NuHistDb, DB>(db).await,
+            Self::Xonsh => import::<Xonsh, DB>(db).await,
+            Self::XonshSqlite => import::<XonshSqlite, DB>(db).await,
         }
     }
 }
