63a361fd54093fc65e67dd2468a257b6efc41761
Ellie Huxtable <ellie@elliehuxtable.com>
2024-02-15 16:16:26 +0000

    omfg fmt too

--- atuin/src/command/client/config/alias.rs
+++ atuin/src/command/client/config/alias.rs
@@ -13,22 +13,13 @@ pub enum Cmd {
 }
 
 impl Cmd {
-    async fn set(
-        &self,
-        store: AliasStore,
-        name: String,
-        value: String,
-    ) -> Result<()> {
+    async fn set(&self, store: AliasStore, name: String, value: String) -> Result<()> {
         store.set(&name, &value).await?;
 
         Ok(())
     }
 
-    async fn delete(
-        &self,
-        store: AliasStore,
-        name: String,
-    ) -> Result<()> {
+    async fn delete(&self, store: AliasStore, name: String) -> Result<()> {
         store.delete(&name).await?;
 
         Ok(())
@@ -43,15 +34,9 @@ impl Cmd {
         let alias_store = AliasStore::new(store, host_id, encryption_key);
 
         match self {
-            Self::Set { name, value } => {
-                self.set(alias_store, name.clone(), value.clone())
-                    .await
-            }
+            Self::Set { name, value } => self.set(alias_store, name.clone(), value.clone()).await,
 
-            Self::Delete{ name } => {
-                self.delete(alias_store, name.clone())
-                    .await
-            }
+            Self::Delete { name } => self.delete(alias_store, name.clone()).await,
         }
     }
 }
