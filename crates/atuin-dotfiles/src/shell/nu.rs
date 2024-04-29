5891e387b9f4bfa4d67cb35eee89cea9c9c0a3aa
YummyOreo <bobgim20@gmail.com>
2024-04-28 21:42:35 -0500

    refactor: remove useless comment

--- crates/atuin-dotfiles/src/shell/nu.rs
+++ crates/atuin-dotfiles/src/shell/nu.rs
@@ -2,6 +2,5 @@ pub async fn var_config() -> String {
     // Because nushell won't autoupdate, we just parse the output of `atuin dotfiles var list` in
     // nushell and load the env vars that way
 
-    // we should only do this if the dotfiles are enabled
     String::from(r#"atuin dotfiles var list | lines | parse "export {name}={value}" | reduce -f {} {|it, acc| $acc | upsert $it.name $it.value} | load-env"#)
 }
