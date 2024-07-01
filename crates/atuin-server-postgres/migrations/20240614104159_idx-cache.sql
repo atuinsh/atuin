a77664401b818857030f2fadabb2153c34cf7b3b
Ellie Huxtable <ellie@elliehuxtable.com>
2024-07-01 13:41:04 +0100

    indentation

--- crates/atuin-server-postgres/migrations/20240614104159_idx-cache.sql
+++ crates/atuin-server-postgres/migrations/20240614104159_idx-cache.sql
@@ -15,27 +15,27 @@ create or replace function cache_store_idx()
 returns trigger as 
 $func$
 begin
-	if (TG_OP='INSERT') then
-		update store_idx_cache set idx = (select max(idx) from store where user_id=new.user_id and host=new.host and tag=new.tag) where user_id=new.user_id and host=new.host and tag=new.tag;
+  if (TG_OP='INSERT') then
+    update store_idx_cache set idx = (select max(idx) from store where user_id=new.user_id and host=new.host and tag=new.tag) where user_id=new.user_id and host=new.host and tag=new.tag;
 
-		if not found then
-			insert into store_idx_cache(user_id, host, tag, idx) 
-			values (
-				new.user_id, 
+    if not found then
+      insert into store_idx_cache(user_id, host, tag, idx) 
+      values (
+        new.user_id, 
         new.host,
         new.tag,
-				(select max(idx) from store where user_id = new.user_id and host=new.host and tag=new.tag)
-			);
-		end if;
-	end if;
+        (select max(idx) from store where user_id = new.user_id and host=new.host and tag=new.tag)
+      );
+    end if;
+  end if;
 
-	return NEW; -- this is actually ignored for an after trigger, but oh well
+  return NEW; -- this is actually ignored for an after trigger, but oh well
 end;
 $func$
 language plpgsql volatile -- pldfplplpflh
 cost 100; -- default value
 
 create or replace trigger tg_cache_store_idx
-	after insert on store
-	for each row 
-	execute procedure cache_store_idx();
+after insert on store
+for each row 
+execute procedure cache_store_idx();
