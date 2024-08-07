0d4ab821c1917894a80a3b4f9ae0c247e07b5f3d
Ellie Huxtable <ellie@elliehuxtable.com>
2024-08-07 17:20:31 +0100

    update flake shasum

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             fenix.packages.${system}.fromToolchainFile
             {
               file = ./rust-toolchain.toml;
-              sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+              sha256 = "sha256-6eN/GKzjVSjEhGO9FhWObkRFaE1Jf+uqMSdQnb8lcB4=";
             };
         in
           pkgs.makeRustPlatform {
