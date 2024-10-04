b331eef4df77a926f721439eda0fe63ef0ff6191
lilydjwg <lilydjwg@gmail.com>
2024-10-04 22:58:00 +0800

    feat: option to include duplicate commands when printing history commands

--- crates/atuin/src/command/client/search.rs
+++ crates/atuin/src/command/client/search.rs
@@ -119,6 +119,10 @@ pub struct Cmd {
     /// Set the maximum number of lines Atuin's interface should take up.
     #[arg(long = "inline-height")]
     inline_height: Option<u16>,
+
+    /// Include duplicate commands in the output (non-interactive only)
+    #[arg(long)]
+    include_duplicates: bool,
 }
 
 impl Cmd {
@@ -215,6 +219,7 @@ impl Cmd {
                 limit: self.limit,
                 offset: self.offset,
                 reverse: self.reverse,
+                include_duplicates: self.include_duplicates,
             };
 
             let mut entries =
