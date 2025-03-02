5cccb331e408b67b41375d1a25a33b64875269c8
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-03-02 16:29:41 +0100

    feat(powershell): add dotfiles support

--- crates/atuin/src/command/client/init/powershell.rs
+++ crates/atuin/src/command/client/init/powershell.rs
@@ -1,3 +1,5 @@
+use atuin_dotfiles::store::{var::VarStore, AliasStore};
+
 pub fn init_static(disable_up_arrow: bool, disable_ctrl_r: bool) {
     let base = include_str!("../../../shell/atuin.ps1");
 
@@ -13,6 +15,23 @@ pub fn init_static(disable_up_arrow: bool, disable_ctrl_r: bool) {
         ps_bool(bind_ctrl_r),
         ps_bool(bind_up_arrow)
     );
+}
+
+pub async fn init(
+    aliases: AliasStore,
+    vars: VarStore,
+    disable_up_arrow: bool,
+    disable_ctrl_r: bool,
+) -> eyre::Result<()> {
+    init_static(disable_up_arrow, disable_ctrl_r);
+
+    let aliases = atuin_dotfiles::shell::powershell::alias_config(&aliases).await;
+    let vars = atuin_dotfiles::shell::powershell::var_config(&vars).await;
+
+    println!("{aliases}");
+    println!("{vars}");
+
+    Ok(())
 }
 
 fn ps_bool(value: bool) -> &'static str {
