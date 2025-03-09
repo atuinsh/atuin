6ae0b3fa4aae2901a0387cda701d3aeecc7064d9
chitao1234 <1139954766@qq.com>
2025-03-10 06:29:55 +0800

    feat: make new arrow key behavior configurable (#2606)
    * feat: make new arrow key behavior configurable
    
    The arrow key behavior in interactive search was changed in #2453,
    make it configurable via keys.exit_past_line_start and
    keys.accept_past_line_end
    
    * Update crates/atuin-client/config.toml
    
    * Update crates/atuin-client/config.toml
    
    * Update crates/atuin-client/config.toml
    
    * I've made so many typos with these, sorry
    
    ---------
    
    Co-authored-by: Ellie Huxtable <ellie@elliehuxtable.com>

--- crates/atuin/src/command/client/search/interactive.rs
+++ crates/atuin/src/command/client/search/interactive.rs
@@ -224,10 +224,12 @@ impl State {
             KeyCode::Esc if esc_allow_exit => Some(Self::handle_key_exit(settings)),
             KeyCode::Char('[') if ctrl && esc_allow_exit => Some(Self::handle_key_exit(settings)),
             KeyCode::Tab => Some(InputAction::Accept(self.results_state.selected())),
-            KeyCode::Right if cursor_at_end_of_line => {
+            KeyCode::Right if cursor_at_end_of_line && settings.keys.accept_past_line_end => {
                 Some(InputAction::Accept(self.results_state.selected()))
             }
-            KeyCode::Left if cursor_at_start_of_line => Some(Self::handle_key_exit(settings)),
+            KeyCode::Left if cursor_at_start_of_line && settings.keys.exit_past_line_start => {
+                Some(Self::handle_key_exit(settings))
+            }
             KeyCode::Char('o') if ctrl => {
                 self.tab_index = (self.tab_index + 1) % TAB_TITLES.len();
                 Some(InputAction::Continue)
