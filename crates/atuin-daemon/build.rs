99f5c995de4ed674ef12a2d8e2b9976c6a6153d4
Shroomy Shroom <sporeventexplosion@gmail.com>
2025-03-22 19:03:20 +0000

    fix(build): change atuin-daemon build script .proto paths
    Modify paths specified in atuin-daemon build.rs to make tonic-build
    print the correct cargo:rerun-if-changed instructions.
    
    Fixes atuin-daemon being rebuilt unconditionally, even if .proto files
    are unchanged.

--- crates/atuin-daemon/build.rs
+++ crates/atuin-daemon/build.rs
@@ -3,7 +3,10 @@ use std::{env, fs, path::PathBuf};
 use protox::prost::Message;
 
 fn main() -> std::io::Result<()> {
-    let file_descriptors = protox::compile(["history.proto"], ["./proto/"]).unwrap();
+    let proto_paths = ["proto/history.proto"];
+    let proto_include_dirs = ["proto"];
+
+    let file_descriptors = protox::compile(proto_paths, proto_include_dirs).unwrap();
 
     let file_descriptor_path = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"))
         .join("file_descriptor_set.bin");
@@ -13,5 +16,5 @@ fn main() -> std::io::Result<()> {
         .build_server(true)
         .file_descriptor_set_path(&file_descriptor_path)
         .skip_protoc_run()
-        .compile_protos(&["history.proto"], &["."])
+        .compile_protos(&proto_paths, &proto_include_dirs)
 }
