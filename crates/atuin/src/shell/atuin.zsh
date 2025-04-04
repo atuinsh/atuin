atuinsh/atuin

commit 264da9e4e964512f1e2e814c76010fd11d31b87a

feat(zsh): try to go to the position in zsh's history (#1469)
by using infer-next-history and then up-history.

This is very helpful to execute consecutive commands with
accept-line-and-down-history.

crates/atuin/src/shell/atuin.zsh
@@ -68,6 +68,8 @@ _atuin_search() {
         then
             LBUFFER=${LBUFFER#__atuin_accept__:}
             zle accept-line
+        else
+            zle infer-next-history && zle up-history
         fi
     fi
 }

commit a3fe5d738996a475eeafd9496a186dfd680e663a

fix(zsh): avoid calling user-defined widgets when searching for history position
Or those widgets may behave badly when calling them too quickly. We don't need calling them anyway as this is not considered to be user action, and if our calls fail, the history position is unchanged.

Issue introduced in #1469.

crates/atuin/src/shell/atuin.zsh
@@ -72,7 +72,7 @@ _atuin_search() {
             LBUFFER=${LBUFFER#__atuin_accept__:}
             zle accept-line
         else
-            zle infer-next-history && zle up-history
+            zle .infer-next-history && zle .up-history
         fi
     fi
 }