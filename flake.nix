d77c8ec2ba81e89473fbe91c0a96d8681d30577f
Bloxx12 <charlie@charlieroot.dev>
2025-06-10 19:41:38 +0200

    flake: drop flake utils, cleanup

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -14,59 +13,78 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     fenix,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = nixpkgs.outputs.legacyPackages.${system};
-    in {
-      packages.atuin = pkgs.callPackage ./atuin.nix {
-        inherit (pkgs.darwin.apple_sdk.frameworks) Security SystemConfiguration AppKit;
-        rustPlatform = let
-          toolchain =
-            fenix.packages.${system}.fromToolchainFile
-            {
-              file = ./rust-toolchain.toml;
-              sha256 = "sha256-X/4ZBHO3iW0fOenQ3foEvscgAPJYl2abspaBThDOukI=";
-            };
-        in
-          pkgs.makeRustPlatform {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-      };
-      packages.default = self.outputs.packages.${system}.atuin;
-
-      devShells.default = self.packages.${system}.default.overrideAttrs (super: {
-        nativeBuildInputs = with pkgs;
-          super.nativeBuildInputs
-          ++ [
-            cargo-edit
-            clippy
-            rustfmt
-          ];
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-
-        shellHook = ''
-          echo >&2 "Setting development database path"
-          export ATUIN_DB_PATH="/tmp/atuin_dev.db"
-          export ATUIN_RECORD_STORE_PATH="/tmp/atuin_records.db"
-
-          if [ -e "''${ATUIN_DB_PATH}" ]; then
-            echo >&2 "''${ATUIN_DB_PATH} already exists, you might want to double-check that"
-          fi
-
-          if [ -e "''${ATUIN_RECORD_STORE_PATH}" ]; then
-            echo >&2 "''${ATUIN_RECORD_STORE_PATH} already exists, you might want to double-check that"
-          fi
-        '';
+  }: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    eachSystem = lib.genAttrs systems;
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = [self.overlays.atuin];
       });
-    })
-    // {
-      overlays.default = final: prev: {
-        inherit (self.packages.${final.system}) atuin;
+  in {
+    packages = eachSystem (system: {
+      inherit (pkgsFor.${system}) atuin;
+
+      default = self.packages.${system}.atuin;
+    });
+
+    devShells =
+      lib.mapAttrs (
+        system: pkgs: {
+          default = self.packages.${system}.default.overrideAttrs (super: {
+            nativeBuildInputs = with pkgs;
+              super.nativeBuildInputs
+              ++ [
+                cargo-edit
+                clippy
+                rustfmt
+              ];
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+            shellHook = ''
+              echo >&2 "Setting development database path"
+              export ATUIN_DB_PATH="/tmp/atuin_dev.db"
+              export ATUIN_RECORD_STORE_PATH="/tmp/atuin_records.db"
+
+              if [ -e "''${ATUIN_DB_PATH}" ]; then
+                echo >&2 "''${ATUIN_DB_PATH} already exists, you might want to double-check that"
+              fi
+
+              if [ -e "''${ATUIN_RECORD_STORE_PATH}" ]; then
+                echo >&2 "''${ATUIN_RECORD_STORE_PATH} already exists, you might want to double-check that"
+              fi
+            '';
+          });
+        }
+      )
+      pkgsFor;
+
+    overlays = {
+      atuin = final: _: let
+        toolchain = fenix.packages.${final.system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-X/4ZBHO3iW0fOenQ3foEvscgAPJYl2abspaBThDOukI=";
+        };
+        rustPlatform = final.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in {
+        atuin = final.callPackage ./atuin.nix {
+          inherit rustPlatform;
+          inherit (final.darwin.apple_sdk.frameworks) Security SystemConfiguration AppKit;
+        };
+
+        default = self.overlays.atuin;
       };
     };
+  };
 }
