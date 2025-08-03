f65cbf2188e03bf3438913ee114ba716bce793a3
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-08-03 20:08:40 +0200

    fix: clippy issues on Windows

--- crates/atuin/src/command/client/search/interactive.rs
+++ crates/atuin/src/command/client/search/interactive.rs
@@ -34,8 +34,7 @@ use ratatui::{
         cursor::SetCursorStyle,
         event::{
             self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers,
-            KeyboardEnhancementFlags, MouseEvent, PopKeyboardEnhancementFlags,
-            PushKeyboardEnhancementFlags,
+            MouseEvent,
         },
         execute, terminal,
     },
@@ -44,6 +43,11 @@ use ratatui::{
     style::{Modifier, Style},
     text::{Line, Span, Text},
     widgets::{Block, BorderType, Borders, Padding, Paragraph, Tabs, block::Title},
+};
+
+#[cfg(not(target_os = "windows"))]
+use ratatui::crossterm::event::{
+    KeyboardEnhancementFlags, PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
 };
 
 const TAB_TITLES: [&str; 2] = ["Search", "Inspect"];
@@ -1303,6 +1307,7 @@ mod tests {
     use super::State;
 
     #[test]
+    #[allow(clippy::too_many_lines)]
     fn calc_preview_height_test() {
         let settings_preview_auto = Settings {
             preview: Preview {
