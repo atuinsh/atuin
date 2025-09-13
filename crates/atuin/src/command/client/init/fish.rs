0dd4d6ce262e3bb27480902ee617f35b78bd5e60
r-vdp <ramses@well-founded.dev>
2025-09-13 11:36:33 +0200

    fix: fish up binding
    https://github.com/atuinsh/atuin/pull/2677 introduced logic based on
    `$FISH_VERSION` to determine how to bind keys in fish.
    However, this env var is not exported to child processes, as you can see
    by running `sh -c 'echo $FISH_VERSION'` from fish.
    
    Instead, we can simply push the if statement into the actual fish code
    that we generate, since fish is aware of its own version, and then we
    can decide which binding syntax to use.
    
    Co-authored-by: Anson VanDoren <anson.vandoren@gmail.com>

--- crates/atuin/src/command/client/init/fish.rs
+++ crates/atuin/src/command/client/init/fish.rs
@@ -10,30 +10,27 @@ pub fn init_static(disable_up_arrow: bool, disable_ctrl_r: bool) {
     // We keep it for compatibility with fish 3.x
     if std::env::var("ATUIN_NOBIND").is_err() {
         const BIND_CTRL_R: &str = r"bind \cr _atuin_search";
+        const BIND_UP_ARROW: &str = r"if string match -q '4.*' $version
+    bind up _atuin_bind_up
+else
+    bind -k up _atuin_bind_up
+end
+bind \eOA _atuin_bind_up
+bind \e\[A _atuin_bind_up";
         const BIND_CTRL_R_INS: &str = r"bind -M insert \cr _atuin_search";
-        const BIND_UP_ARROW_INS: &str = r"bind -M insert -k up _atuin_bind_up
+        const BIND_UP_ARROW_INS: &str = r"if string match -q '4.*' $version
+    bind -M insert up _atuin_bind_up
+else
+    bind -M insert -k up _atuin_bind_up
+end
 bind -M insert \eOA _atuin_bind_up
 bind -M insert \e\[A _atuin_bind_up";
-
-        let bind_up_arrow = match std::env::var("FISH_VERSION") {
-            Ok(ref version) if version.starts_with("4.") => r"bind up _atuin_bind_up",
-            Ok(_) => r"bind -k up _atuin_bind_up",
-
-            // do nothing - we can't panic or error as this could be in use in
-            // non-fish pipelines
-            _ => "",
-        }
-        .to_string();
 
         if !disable_ctrl_r {
             println!("{BIND_CTRL_R}");
         }
         if !disable_up_arrow {
-            println!(
-                r"{bind_up_arrow}
-bind \eOA _atuin_bind_up
-bind \e\[A _atuin_bind_up"
-            );
+            println!("{BIND_UP_ARROW}");
         }
 
         println!("if bind -M insert > /dev/null 2>&1");
