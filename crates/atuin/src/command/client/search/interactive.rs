b71358b6869a86a9dcb0b3d9c017979396ac4f54
Phil Weir <phil.weir@flaxandteal.co.uk>
2025-09-21 20:12:24 +0100

    fix(theming): use theme for bolded text, and fallback to 'default' not '(none)' to ensure it is used

--- crates/atuin/src/command/client/search/interactive.rs
+++ crates/atuin/src/command/client/search/interactive.rs
@@ -764,7 +764,7 @@ impl State {
                 .block(Block::default().borders(Borders::NONE))
                 .select(self.tab_index)
                 .style(Style::default())
-                .highlight_style(Style::default().bold().white().on_black());
+                .highlight_style(theme.as_style(Meaning::Important));
 
             f.render_widget(tabs, tabs_chunk);
         }
