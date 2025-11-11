7e696f6b7dd0271eb4848aed9b060ca9124a2879
Marc Jakobi <marc.jakobi@tiko.energy>
2025-11-11 09:32:41 +0100

    build(nix): update rust toolchain hash

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
                 fenix.packages.${system}.fromToolchainFile
                   {
                     file = ./rust-toolchain.toml;
-                    sha256 = "sha256-2eWc3xVTKqg5wKSHGwt1XoM/kUBC6y3MWfKg74Zn+fY=";
+                    sha256 = "sha256-SDu4snEWjuZU475PERvu+iO50Mi39KVjqCeJeNvpguU=";
                   };
             in
             pkgs.makeRustPlatform {
