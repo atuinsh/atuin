70b4613c741a727ca6769fb166803d419b2d3e74
lilydjwg <lilydjwg@gmail.com>
2026-01-25 00:26:06 +0800

    feat(ui): highlight fulltext search as fulltext search instead of fuzzy search

--- crates/atuin/src/command/client/search/engines/db.rs
+++ crates/atuin/src/command/client/search/engines/db.rs
@@ -36,6 +36,8 @@ impl SearchEngine for Search {
     fn get_highlight_indices(&self, command: &str, search_input: &str) -> Vec<usize> {
         if self.0 == SearchMode::Prefix {
             return vec![];
+        } else if self.0 == SearchMode::FullText {
+            return get_highlight_indices_fulltext(command, search_input);
         }
         let mut fzf = FzfV2::new();
         let mut parser = FzfParser::new();
@@ -46,4 +48,69 @@ impl SearchEngine for Search {
         // convert ranges to all indices
         ranges.into_iter().flatten().collect()
     }
+}
+
+fn get_highlight_indices_fulltext(command: &str, search_input: &str) -> Vec<usize> {
+    let mut ranges = vec![];
+    let mut is_regex = false;
+    let uncased_command = command.to_ascii_lowercase();
+    for part in search_input.split_inclusive(' ') {
+        let match_part = match (is_regex, part.starts_with("r/")) {
+            (false, false) => {
+                if part.trim_end().is_empty() {
+                    continue;
+                }
+                part.trim_end()
+            }
+            (false, true) => {
+                is_regex = true;
+                continue;
+            }
+            (true, _) => {
+                if part.trim_end().ends_with('/') {
+                    is_regex = false;
+                }
+                continue;
+            }
+        };
+
+        if match_part.starts_with('!') {
+            continue;
+        };
+
+        let case_sensitive = match_part.contains(char::is_uppercase);
+        let mut at_start = false;
+        let mut at_end = false;
+
+        let match_part = if let Some(term) = match_part.strip_prefix('^') {
+            at_start = true;
+            term
+        } else if let Some(term) = match_part.strip_suffix('$') {
+            at_end = true;
+            term
+        } else {
+            match_part
+        };
+
+        let matchee = if case_sensitive {
+            command
+        } else {
+            &uncased_command
+        };
+        if at_start && matchee.starts_with(match_part) {
+            ranges.push(0..match_part.len());
+        } else if at_end && matchee.ends_with(match_part) {
+            let l = matchee.len();
+            ranges.push((l - match_part.len())..l);
+        } else {
+            for (idx, m) in matchee.match_indices(match_part) {
+                ranges.push(idx..(idx + m.len()));
+            }
+        }
+    }
+
+    let mut ret: Vec<_> = ranges.into_iter().flatten().collect();
+    ret.sort_unstable();
+    ret.dedup();
+    ret
 }
