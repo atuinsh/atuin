c83f6ca37a496bdfb67e40139451cdcb5b8c8ef4
Ellie Huxtable <ellie@elliehuxtable.com>
2026-02-24 16:09:35 -0800

    Remove history author migration backfill

--- crates/atuin-client/migrations/20260224000100_history_author_intent.sql
+++ crates/atuin-client/migrations/20260224000100_history_author_intent.sql
@@ -1,9 +1,2 @@
 alter table history add column author text;
 alter table history add column intent text;
-
-update history
-set author = case
-    when instr(hostname, ':') > 0 then substr(hostname, instr(hostname, ':') + 1)
-    else hostname
-end
-where author is null or trim(author) = '';
