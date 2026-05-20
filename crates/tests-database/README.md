0a15503af676064502fda1ad46834847481c800f
John Oxley <john.oxley@gmail.com>
2026-05-20 16:09:23 +0100

    feature: create database integration tests for atuin-server

---
+++ crates/tests-database/README.md
@@ -0,0 +1,58 @@
+# Database Integration Tests
+
+tl;dr Run `cargo test` to exercise the SQLite database using a memory database
+
+This is an integration test suite that runs through a simple "story" of creating a user, adding some history, adding some records and doing some deleting.  The idea is to ensure that a database implementation does the right thing.
+
+## `ATUIN_TEST_DB_URI`
+Setting this will create a database at that URI and then run through the tests.  Leaving it unset will use `sqlite::memory:` as the database.
+
+There will be a [snowflake_uid](https://docs.rs/snowflake_uid/latest/snowflake_uid/) appended to the end of the URL so the DB will be unique for the test
+
+## Postgres
+
+```shell
+ATUIN_TEST_DB_URI=postgres://postgres:pg@localhost/atuin_test_ cargo test
+```
+
+Will create a database and run the tests on it.
+
+If you want to quickly and easily run a postgres instance 
+
+```shell
+podman run --name atuin-pg-test -e POSTGRES_PASSWORD=pg -p 5432:5432 postgres
+```
+
+## Help, I want to see what was left over
+
+Set the environment variable `ATUIN_TEST_DB_NO_DESTROY` to anything and run the tests, e.g.
+
+```shell
+ATUIN_TEST_DB_NO_DESTROY=1 ATUIN_TEST_DB_URI=postgres://postgres:pg@localhost/atuin_test_ cargo test
+```
+
+will give
+
+```plain
+postgres@127.0.0.1:postgres> \l
++--------------------------------+----------+----------+------------+------------+-----------------------+
+| Name                           | Owner    | Encoding | Collate    | Ctype      | Access privileges     |
+|--------------------------------+----------+----------+------------+------------+-----------------------|
+| atuin_test_3734160738059550720 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | <null>                |
+```
+
+## Design Decision
+This is intentionally not an integration test in `atuin-server-database` because it calls into the database implementation crates and I did not want to introduce a circular depdendency
+
+## Glossary
+
+
+| Term | Definition |
+| :--- | :--- |
+| user | You, yes you looking at this.  It's you! |
+| session | a working session |
+| record | ... | 
+| history | not sure what the difference between this and record is |
+| store | shrug |
+
+
---
+++ crates/tests-database/tests/db_story.rs
@@ -0,0 +1,231 @@
+use std::env;
+
+use atuin_common::{
+    record::{EncryptedData, Host, HostId, Record, RecordIdx},
+    utils::{crypto_random_string, uuid_v7},
+};
+use atuin_server_database::{
+    Database, DbSettings, DbType,
+    models::{NewHistory, NewSession, NewUser, User},
+};
+use atuin_server_postgres::Postgres;
+use atuin_server_sqlite::Sqlite;
+use snowflake_uid::{Config, Generator};
+use sqlx::migrate::MigrateDatabase;
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+/// Create a test database. You need to supply a database URL from the environment variable
+/// ATUIN_TEST_DB_URI.  It will default to `sqlite::memory:` if not specified
+/// If specified, a snowflake UUID will be added to the end of the URL so the test is unieuq
+/// it so `test_atuin_` will become `test_ation_<numbers>`
+/// postgres://user:pass@host/test_atuin_
+/// The DB user must have permissions to create a database
+async fn create_test_db_settings() -> eyre::Result<DbSettings> {
+    let db_uri = env::var("ATUIN_TEST_DB_URI")
+        .map(|uri| {
+            let cfg = Config::default();
+            let mut generator = Generator::from(cfg, 0);
+            let uid = generator.get();
+            format!("{uri}{uid}")
+        })
+        .unwrap_or_else(|_| "sqlite::memory:".to_owned());
+
+    let settings = DbSettings {
+        db_uri,
+        read_db_uri: None,
+    };
+
+    match settings.db_type() {
+        DbType::Postgres => sqlx::Postgres::create_database(&settings.db_uri).await?,
+        DbType::Sqlite => (),
+        DbType::Unknown => unimplemented!(),
+    }
+
+    Ok(settings)
+}
+
+/// This will destroy the database unless ATUIN_TEST_DB_NO_DESTROY is set
+async fn destroy_test_db(settings: &DbSettings) -> eyre::Result<()> {
+    if env::var("ATUIN_TEST_DB_NO_DESTROY").is_ok() {
+        // var is set so bail early
+        return Ok(());
+    }
+    match settings.db_type() {
+        DbType::Postgres => Ok(sqlx::Postgres::drop_database(&settings.db_uri).await?),
+        DbType::Sqlite => Ok(sqlx::Sqlite::drop_database(&settings.db_uri).await?),
+        DbType::Unknown => Ok(()),
+    }
+}
+
+/// This test runs through a story of using the database. The goal is to fully exercise all DB code
+/// in a single repeatable manner.
+#[tokio::test]
+async fn test_full_db_story() -> eyre::Result<()> {
+    let settings = create_test_db_settings().await?;
+
+    let result = match settings.db_type() {
+        DbType::Postgres => run_the_test::<Postgres>(&settings).await,
+        DbType::Sqlite => run_the_test::<Sqlite>(&settings).await,
+        DbType::Unknown => todo!(),
+    };
+    destroy_test_db(&settings).await?;
+    result
+}
+
+async fn run_the_test<DB: Database>(settings: &DbSettings) -> eyre::Result<()> {
+    let db = DB::new(settings).await?;
+    // register a user
+    let new_user = NewUser {
+        username: "foo".to_owned(),
+        email: "foo@example.com".to_owned(),
+        password: "hunter2".to_owned(),
+    };
+    let user_id = db.add_user(&new_user).await?;
+    assert_ne!(user_id, 0);
+
+    let token = crypto_random_string::<24>();
+    let new_session = NewSession {
+        user_id,
+        token: token.clone(),
+    };
+    db.add_session(&new_session).await?;
+
+    // The user is now registered and has a session. This happens when a user logs in
+    let user = db.get_session_user(&token).await?;
+    assert_eq!(user.username, "foo");
+
+    let session = db.get_session(&token).await?;
+    assert_eq!(session.user_id, user_id);
+
+    let user = db.get_user("foo").await?;
+    assert_eq!(user.password, "hunter2");
+
+    // Lets change the password
+    let user = User {
+        email: "foo@example.com".to_owned(),
+        id: user_id,
+        password: "hunter3".to_owned(),
+        username: "foo".to_owned(),
+    };
+    db.update_user_password(&user).await?;
+
+    let user = db.get_user("foo").await?;
+    assert_eq!(user.password, "hunter3");
+
+    // add some history
+    let h = vec![
+        generate_history(user_id),
+        generate_history(user_id),
+        generate_history(user_id),
+        generate_history(user_id),
+    ];
+    db.add_history(&h).await?;
+
+    assert_eq!(db.count_history(&user).await?, 4);
+
+    // AFAICT history is not used any more so I'm not going figure out how to take the timestamps
+    // from generated history into this
+    // assert_eq!(db.count_history_range(&user).await?, 4);
+
+    db.delete_history(&user, h[0].client_id.clone()).await?;
+    let deleted_history = db.deleted_history(&user).await?;
+    assert_eq!(deleted_history.len(), 1);
+
+    // add a bunch of records
+    let host_a = Host::new(HostId(uuid_v7()));
+    let host_b = Host::new(HostId(uuid_v7()));
+    let records = vec![
+        generate_record(&host_a, 1),
+        generate_record(&host_b, 2),
+        generate_record(&host_a, 2),
+        generate_record(&host_b, 2),
+        generate_record(&host_a, 3),
+        generate_record(&host_a, 4),
+        generate_record(&host_a, 5),
+        generate_record(&host_a, 6),
+    ];
+    db.add_records(&user, &records).await?;
+
+    let status = db.status(&user).await?;
+    assert!(status.hosts.contains_key(&host_a.id));
+    assert!(status.hosts.contains_key(&host_b.id));
+    assert_eq!(
+        status
+            .hosts
+            .get(&host_a.id)
+            .unwrap()
+            .get("history")
+            .unwrap()
+            .clone(),
+        6
+    );
+    assert_eq!(
+        status
+            .hosts
+            .get(&host_b.id)
+            .unwrap()
+            .get("history")
+            .unwrap()
+            .clone(),
+        2
+    );
+
+    // Get 3 records from the beginning
+    let recs = db
+        .next_records(&user, host_a.id, "history".into(), None, 3)
+        .await?;
+    assert_eq!(recs.len(), 3);
+    assert_eq!(recs[0].idx, 1);
+    assert_eq!(recs.last().unwrap().idx, 3);
+
+    // Get from the end, for host a. Get more than exists
+    let recs = db
+        .next_records(&user, host_a.id, "history".into(), Some(4), 10)
+        .await?;
+    assert_eq!(recs.len(), 3);
+    assert_eq!(recs[0].idx, 4); // check the head record is idx 4
+    assert_eq!(recs.last().unwrap().idx, 6);
+
+    // delete_store
+    db.delete_store(&user).await?;
+    let recs = db
+        .next_records(&user, host_a.id, "history".into(), Some(4), 10)
+        .await?;
+    assert_eq!(recs.len(), 0);
+
+    Ok(())
+}
+
+fn generate_history(user_id: i64) -> NewHistory {
+    use fake::Fake;
+    use fake::faker::lorem::en::*;
+
+    let data: String = Sentence(1..3).fake();
+    let hostname: String = "foo".to_owned();
+    let client_id = Uuid::new_v4().to_string();
+
+    let timestamp: OffsetDateTime = OffsetDateTime::now_utc();
+
+    NewHistory {
+        client_id,
+        user_id,
+        hostname,
+        timestamp,
+        data,
+    }
+}
+
+fn generate_record(host: &Host, idx: RecordIdx) -> Record<EncryptedData> {
+    let data = EncryptedData {
+        data: "some data".into(),
+        content_encryption_key: "key".into(),
+    };
+    Record::builder()
+        .idx(idx)
+        .host(host.clone())
+        .version("2".into())
+        .tag("history".into())
+        .data(data)
+        .build()
+}
