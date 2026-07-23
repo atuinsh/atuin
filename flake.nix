2f9357e7246dbfb342a8807089e8dac3e60afec9
Eric Shen <ericshen118@gmail.com>
2026-07-23 16:41:06 -0700

    chore: update flake.nix (#3509)
    Co-authored-by: Marko Vejnovic <marko@atuin.sh>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -12,63 +12,66 @@
     };
   };
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , fenix
-    , ...
-    }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = nixpkgs.outputs.legacyPackages.${system};
-      in
-      {
-        packages.atuin = pkgs.callPackage ./atuin.nix {
-          rustPlatform =
-            let
-              toolchain =
-                fenix.packages.${system}.fromToolchainFile
-                  {
-                    file = ./rust-toolchain.toml;
-                    sha256 = "sha256-OATSZm98Es5kIFuqaba+UvkQtFsVgJEBMmS+t6od5/U=";
-                  };
-            in
-            pkgs.makeRustPlatform {
+    inputs@{ flake-parts, fenix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-OATSZm98Es5kIFuqaba+UvkQtFsVgJEBMmS+t6od5/U=";
+          };
+          atuin = pkgs.callPackage ./atuin.nix {
+            rustPlatform = pkgs.makeRustPlatform {
               cargo = toolchain;
               rustc = toolchain;
             };
+          };
+        in
+        {
+          packages = {
+            inherit atuin;
+            default = atuin;
+          };
+
+          formatter = pkgs.nixfmt;
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ atuin ];
+
+            nativeBuildInputs =
+              (with pkgs; [
+                cargo-edit
+                rust-analyzer
+              ])
+              ++ [ toolchain ];
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+            shellHook = ''
+              echo >&2 "Setting development database path"
+              export ATUIN_DB_PATH="/tmp/atuin_dev.db"
+              export ATUIN_RECORD_STORE_PATH="/tmp/atuin_records.db"
+
+              if [ -e "''${ATUIN_DB_PATH}" ]; then
+                echo >&2 "''${ATUIN_DB_PATH} already exists, you might want to double-check that"
+              fi
+
+              if [ -e "''${ATUIN_RECORD_STORE_PATH}" ]; then
+                echo >&2 "''${ATUIN_RECORD_STORE_PATH} already exists, you might want to double-check that"
+              fi
+            '';
+          };
         };
-        packages.default = self.outputs.packages.${system}.atuin;
 
-        devShells.default = self.packages.${system}.default.overrideAttrs (super: {
-          nativeBuildInputs = with pkgs;
-            super.nativeBuildInputs
-            ++ [
-              cargo-edit
-              clippy
-              rustfmt
-            ];
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-
-          shellHook = ''
-            echo >&2 "Setting development database path"
-            export ATUIN_DB_PATH="/tmp/atuin_dev.db"
-            export ATUIN_RECORD_STORE_PATH="/tmp/atuin_records.db"
-
-            if [ -e "''${ATUIN_DB_PATH}" ]; then
-              echo >&2 "''${ATUIN_DB_PATH} already exists, you might want to double-check that"
-            fi
-
-            if [ -e "''${ATUIN_RECORD_STORE_PATH}" ]; then
-              echo >&2 "''${ATUIN_RECORD_STORE_PATH} already exists, you might want to double-check that"
-            fi
-          '';
-        });
-      })
-    // {
-      overlays.default = final: prev: {
-        inherit (self.packages.${final.stdenv.hostPlatform.system}) atuin;
+      flake.overlays.default = final: _: {
+        inherit (inputs.self.packages.${final.stdenv.hostPlatform.system}) atuin;
       };
     };
 }
