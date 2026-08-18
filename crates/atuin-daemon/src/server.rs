708231c00a4ea0238fbb7df3ee3a32c9b60dc0f9
Marko Vejnovic <marko@atuin.sh>
2026-08-18 12:32:07 -0700

    helpful error messages

--- crates/atuin-daemon/src/server.rs
+++ crates/atuin-daemon/src/server.rs
@@ -1,4 +1,4 @@
-use eyre::Result;
+use eyre::{Context, Result};
 
 use crate::components::history::HistoryGrpcService;
 use crate::components::search::SearchGrpcService;
@@ -76,10 +76,15 @@ pub async fn run_grpc_server(
         #[cfg(not(target_os = "linux"))]
         unreachable!()
     } else {
+        use atuin_common::path::DisplayRichExt;
+
         socket_path.create_default_dir_if_needed()?;
         tracing::info!("listening on unix socket {:?}", socket_path.as_path());
         (
-            UnixListener::bind(&socket_path)?,
+            UnixListener::bind(&socket_path).context(format!(
+                "reading socket: {}",
+                socket_path.display_rich().relative_to_cwd()
+            ))?,
             Some(socket_path.into_owned()),
         )
     };
