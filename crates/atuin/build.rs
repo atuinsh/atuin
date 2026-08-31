d54bd038931b8e54bd8ba9ddb8233c659c54d5d8
taylor.fish <contact@taylor.fish>
2026-08-31 15:33:42 -0700

    Normalize `atuin --version` output

--- crates/atuin/build.rs
+++ crates/atuin/build.rs
@@ -3,8 +3,8 @@ fn main() {
     let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
 
     let sha = match output {
-        Ok(sha) => String::from_utf8(sha.stdout).unwrap(),
-        Err(_) => String::from("NO_GIT"),
+        Ok(sha) if sha.status.success() => String::from_utf8(sha.stdout).unwrap(),
+        _ => String::from("NO_GIT"),
     };
 
     println!("cargo:rustc-env=GIT_HASH={sha}");
