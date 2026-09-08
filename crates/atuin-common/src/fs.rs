20537cd23460c20e7ff812107a74b389b46f9e35
taylor.fish <contact@taylor.fish>
2026-09-08 02:10:31 -0700

    derive_more

--- crates/atuin-common/src/fs.rs
+++ crates/atuin-common/src/fs.rs
@@ -1,8 +1,10 @@
 use std::path::{Path, PathBuf};
 
 /// A path that is removed when this type is dropped.
+#[derive(derive_more::AsRef)]
 pub struct RemoveOnDropPath<P: AsRef<Path> = PathBuf>(
     /// The path to the file.
+    #[as_ref(Path)]
     pub P,
 );
 
@@ -12,16 +14,12 @@ impl<P: AsRef<Path>> Drop for RemoveOnDropPath<P> {
     }
 }
 
+// Not using `derive_more` for this as it would require adding a `P: Deref<Target = Path>` bound to
+// the struct.
 impl<P: AsRef<Path>> std::ops::Deref for RemoveOnDropPath<P> {
     type Target = Path;
 
     fn deref(&self) -> &Self::Target {
         self.0.as_ref()
-    }
-}
-
-impl<P: AsRef<Path>> AsRef<Path> for RemoveOnDropPath<P> {
-    fn as_ref(&self) -> &Path {
-        self
     }
 }
